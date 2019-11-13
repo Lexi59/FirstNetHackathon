22fcb9c6ef9c7a5d344eb80cdcea761260122259
Alexis <trip9919@gmail.com>
2019-11-13 11:10:15 -0600

    Initial commit

---
+++ controllers/mobile/HomeController/HomeControllerActions.js
@@ -0,0 +1,11 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for Button0a5ada895c34e4d **/
+    AS_Button_fe5045f8dccc400da3e70691bc95782e: function AS_Button_fe5045f8dccc400da3e70691bc95782e(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("ProfileForm");
+        ntf.navigate();
+    }
+});

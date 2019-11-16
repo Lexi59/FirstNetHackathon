85431c3f7d01731f5aac9fb38e5ccccfc3cf3df8
Alexis <trip9919@gmail.com>
2019-11-15 21:26:55 -0600

    Added More Forms

--- controllers/mobile/HomeController/HomeControllerActions.js
+++ controllers/mobile/HomeController/HomeControllerActions.js
@@ -2,16 +2,16 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
-    /** onClick defined for Button0a5ada895c34e4d **/
-    AS_Button_fe5045f8dccc400da3e70691bc95782e: function AS_Button_fe5045f8dccc400da3e70691bc95782e(eventobject) {
-        var self = this;
-        var ntf = new kony.mvc.Navigation("ProfileForm");
-        ntf.navigate();
-    },
     /** onClick defined for Button0c7863919372f40 **/
     AS_Button_d0cebe97bad54cb78bb84a1754caa1d9: function AS_Button_d0cebe97bad54cb78bb84a1754caa1d9(eventobject) {
         var self = this;
         var ntf = new kony.mvc.Navigation("LoginForm");
+        ntf.navigate();
+    },
+    /** onClick defined for Button0a5ada895c34e4d **/
+    AS_Button_fe5045f8dccc400da3e70691bc95782e: function AS_Button_fe5045f8dccc400da3e70691bc95782e(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("ProfileForm");
         ntf.navigate();
     }
 });
--- controllers/mobile/mapFormController/mapFormControllerActions.js
+++ controllers/mobile/mapFormController/mapFormControllerActions.js
@@ -1,7 +1,17 @@
-define({ 
-
- /* 
- This is an auto generated file and any modifications to it may result in corruption of the action sequence.
- */
-
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for ButtonRoundDoubleLeft **/
+    AS_Button_d3de93570a424fdbbec60d3f855d393e: function AS_Button_d3de93570a424fdbbec60d3f855d393e(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("EventForm");
+        ntf.navigate();
+    },
+    /** onClick defined for ButtonRoundDoubleRight **/
+    AS_Button_b3e1cbed76e84e1ea63eef811fa7b173: function AS_Button_b3e1cbed76e84e1ea63eef811fa7b173(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("MessageForm");
+        ntf.navigate();
+    }
 });
